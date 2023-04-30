6c1ee896103cebc42f75705663412fe7677eb290
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-30 16:56:21 -0700

    remove project images to resize then reupload

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -6,7 +6,12 @@ export default function Work() {
       <h1>Work</h1>
       <Project
         title='Yardsale – C2C Sales (SQL DB)'
-        image='./yardsale.png'
+        image='./assets/yardsale.png'
+        url='https://yardsale-application.herokuapp.com/'
+      />
+      <Project
+        title='Yardsale – C2C Sales (SQL DB)'
+        image='./assets/employee-tracker.png'
         url='https://yardsale-application.herokuapp.com/'
       />
     </div>
