868b35a879cafe7f0f154306e922a43914daa9ad
rhubarb414 <ctophr.dubois@gmail.com>
2023-04-30 18:17:01 -0700

    Adds form skeleton, needs validation

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,49 @@ export default function Contact() {
   return (
     <div className='container'>
       <h1>Contact</h1>
-      <p>Get in touch</p>
+      <p className='red'>***This form is currently non-functioning!***</p>
+      <div class='row'>
+        <form class='col s12'>
+          <div class='row'>
+            <div class='input-field col s6'>
+              <input
+                placeholder='First Name'
+                id='first_name'
+                type='text'
+                class=''
+              ></input>
+              {/* <label for='first_name'></label> */}
+            </div>
+            <div class='input-field col s6'>
+              <input
+                placeholder='Last Name'
+                id='last_name'
+                type='text'
+                class=''
+              ></input>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='input-field col s6'>
+              <input
+                id='email'
+                placeholder='Email'
+                type='email'
+                class=''
+              ></input>
+            </div>
+          </div>
+          <div class='row'>
+            <div class='input-field col s12'>
+              <textarea
+                id='textarea'
+                placeholder='Your Message'
+                class='materialize-textarea'
+              ></textarea>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
